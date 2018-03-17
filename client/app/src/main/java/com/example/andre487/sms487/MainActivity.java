b7431a6e75b165d2113e16aef3847c1fcf31c117
andre487 <andre487@yandex-team.ru>
2018-03-17 23:26:40 +0300

    Create example activity

--- client/app/src/main/java/com/example/andre487/sms487/MainActivity.java
+++ client/app/src/main/java/com/example/andre487/sms487/MainActivity.java
@@ -1,9 +1,26 @@
 package com.example.andre487.sms487;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.andre487.MESSAGE";
+
+    public void sendMessage(View view) {
+        Log.i("DBG", "Send message");
+
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
