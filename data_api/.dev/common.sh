b2f9da505ed7e0e0ceb8de6bb06a2d1cf4fc4df1
andre487 <andre487@yandex-team.ru>
2018-04-07 14:17:46 +0300

    Implement docker testing vie PyRESTTest

--- data_api/.dev/common.sh
+++ data_api/.dev/common.sh
@@ -2,7 +2,7 @@
 export SMS_USER_NAME='test-user'
 export SMS_USER_KEY='5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'
 export MONGO_DB_NAME='sms487_test'
-export AUTH_MISTAKES_TO_BAN=2
+export AUTH_MISTAKES_TO_BAN=1
 
 export FLASK_DEBUG=1
 export FLASK_APP=api.py
