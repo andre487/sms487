3a575c2d2332e6481281b054f3fac152ca2e8a84
andre487 <andre487@yandex-team.ru>
2018-04-07 16:50:11 +0300

    Fix version

--- data_api/templating.py
+++ data_api/templating.py
@@ -9,10 +9,10 @@ def setup_filters(app):
     def static_version_filter(file_name):
         full_path = os.path.join(cur_dir, file_name[1:])
 
+        version = 0
         try:
             version = int(os.path.getmtime(full_path))
         except OSError as e:
             logging.warning('Static version error: %s', e)
-            version = 0
 
         return '{0}?v=0x{1}'.format(file_name, version)
