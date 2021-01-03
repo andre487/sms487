d423895a1200b8eb870dbd1433e79f7883437ea6
Andrey Prokopyuk <andrey.prokopyuk@gmail.com>
2021-01-03 03:50:46 +0300

    Limit Logger messages count in memory

--- client/app/src/main/java/life/andre/sms487/logging/Logger.java
+++ client/app/src/main/java/life/andre/sms487/logging/Logger.java
@@ -4,38 +4,46 @@ import android.util.Log;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ConcurrentLinkedDeque;
 
 public class Logger {
-    private static final ConcurrentLinkedDeque<String> messages = new ConcurrentLinkedDeque<>();
+    public static final int MAX_SIZE = 1024;
+    private static final ArrayList<String> messages = new ArrayList<>();
 
     public static void d(String tag, String msg) {
-        addMessageToQueue("DEBUG", tag, msg);
         Log.d(tag, msg);
+        addMessage("DEBUG", tag, msg);
     }
 
     public static void i(String tag, String msg) {
-        addMessageToQueue("INFO", tag, msg);
         Log.i(tag, msg);
+        addMessage("INFO", tag, msg);
     }
 
     public static void w(String tag, String msg) {
-        addMessageToQueue("WARN", tag, msg);
         Log.w(tag, msg);
+        addMessage("WARN", tag, msg);
     }
 
     public static void e(String tag, String msg) {
-        addMessageToQueue("ERROR", tag, msg);
         Log.e(tag, msg);
+        addMessage("ERROR", tag, msg);
     }
 
     public static List<String> getMessages() {
         return new ArrayList<>(messages);
     }
 
-    private static void addMessageToQueue(String level, String tag, String msg) {
+    private static void addMessage(String level, String tag, String msg) {
         String logLine = level + ": " + tag + ": " + msg;
 
-        messages.addFirst(logLine);
+        synchronized (messages) {
+            messages.add(logLine);
+
+            int toRemove =  messages.size() - MAX_SIZE;
+            while (toRemove > 0) {
+                messages.remove(0);
+                --toRemove;
+            }
+        }
     }
 }
